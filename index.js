a6fa5555350b9b9e6649f9188d12a3d6ada59036
LEE YONGMIN <dldydals1994@gmail.com>
2020-08-15 22:35:31 +0900

    commit

--- index.js
+++ index.js
@@ -10,15 +10,13 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-    var ip = req.headers['x-forwarded-for'] || 
-                req.connection.remoteAddress || 
+    var ip = req.headers['x-forwarded-for'] ||
+                req.connection.remoteAddress ||
                 req.socket.remoteAddress ||
             (req.connection.socket ? req.connection.socket.remoteAddress : null);
     console.log(ip);
     res.send('CLUB MEMBER Management : ' + ip + "," + new Date());
 });
-
-
 // 멤버 개별 조회
 app.get('/Community_Member/:member_id', (req, res) => {
   connection.query('SELECT * from Community_Member WHERE member_id=\'' + req.params.member_id + '\'', (error, rows) => {
@@ -47,8 +45,6 @@ app.get('/health', (req, res) => {
 app.get('/Community_Member', (req, res) => {
   connection.query('SELECT * from Community_Member', (error, rows) => {
     if (error) throw error;
-    console.log('User info is: ', rows);
-    res.send(rows);
   });
 });
 
@@ -63,51 +59,58 @@ app.get('/Travel_Club', (req, res) => {
 
 
 // //회원 가입
-// app.post('/Community_Member_signup/:member_id', (req, res) => {
-//   var member_id=req.params.member_id;
-//   var member_pd=req.body.member_pd;
-//   var member_name=req.body.member_name;
-//   var member_email=req.body.member_email;
-//   var member_phnumber=req.body.member_phnumber;
-  
-//   connection.query("INSERT INTO Community_Member(member_id, member_pd, member_name, member_email, member_phnumber) VALUES(?,?,?,?,?)", 
-//   [member_id, member_pd, member_name, member_email, member_phnumber]
-//   , (error, results, fields) => {
-//     if (error) throw error;
-//     res.json({ok:"member_signup"});
-//   });  
-// });
+app.post('/Community_Member_signup/:member_id', (req, res) => {
+    var member_id=req.body.member_id;
+    var member_pd=req.body.member_pd;
+    var member_name=req.body.member_name;
+    var member_email=req.body.member_email;
+    var member_phnumber=req.body.member_phnumber;
+    
+    
+    connection.query("INSERT INTO Community_Member(member_id, member_pd, member_name, member_email, member_phnumber) VALUES(?,?,?,?,?)", [member_id, member_pd, member_name, member_email, member_phnumber]
+    , (error, results, fields) => {
+      if (error) throw error;
+      res.json({ok:"member_signup"});
+    });  
+  });
 
 
 // //클럽 생성
-// app.post('/Travel_Club/:club_name', (req, res) => {
-//   var club_name=req.params.club_name;
-//   var club_foundationdate=req.body.club_foundationdate;
-//   var club_intro=req.body.club_intro;
-//   connection.query("INSERT INTO Travel_Club(club_name, club_foundationdate, club_intro) VALUES(?,?,?)", [club_name, club_foundationdate, club_intro]
-//   , (error, results, fields) => {
-//     if (error) throw error;
-//     res.json({ok:"club_signup"});
-//   });  
-// });
+app.post('/Travel_Club_signup/:club_name', (req, res) => {
+    var club_name=req.params.club_name;
+    var club_foundationdate=req.body.club_foundationdate;
+    var club_intro=req.body.club_intro;
+    connection.query("INSERT INTO Travel_Club(club_name, club_foundationdate, club_intro) VALUES(?,?,?)",
+    [club_name, club_foundationdate, club_intro], (error, results, fields) => {
+     if (error) throw error;
+     res.json({ok:"club_signup"});
+    });
+});
 
 
 //회원 정보 수정
-// app.post('/Community_Member_update/:member_id', (req, res) => {
-//   var member_id=req.params.member_id;
-//   var member_pd=req.params.member_pd;
-//   var member_name=req.params.member_name;
-//   var member_email=req.params.member_email;
-//   var member_phnumber=req.params.member_phnumber;
+app.put('/Community_Member_update/:member_id', (req, res) => {
+    var member_id=req.body.member_id;
+    var member_pd=req.body.member_pd;
+    var member_name=req.body.member_name;
+    var member_email=req.body.member_email;
+    var member_phnumber=req.body.member_phnumber;
 
-//   connection.query('UPDATE Community_Member SET member_pd="?", member_name="?", member_email="?", member_phnumber="?" WHERE member_id="?"\',
-//    [member_pd, member_name, member_email, member_phnumber, member_id], (error, results, fields) => {
-//     if (error) throw error;
-//     res.json({ok:"club_signup"});
-//     });
-// });
+    connection.query('UPDATE Community_Member SET member_pd="?", member_name="?", member_email="?", member_phnumber="?" WHERE member_id=' + req.params.member_id,
+    [member_pd, member_name, member_email, member_phnumber], (error, results, fields) => {
+     if (error) throw error;
+     res.json({ok:"club_signup"});
+    });
+});
 
-
+// 회원 탈퇴
+app.delete('/Community_Member_delete/:member_id', (req, res) => {
+  connection.query('DELETE FROM Community_Member WHERE member_id=\'' + req.params.member_id + '\'', (error, rows) => {
+    if (error) throw error;
+    console.log('member info is: ', rows);
+    res.send(rows);
+  });
+});
 // app.get('/members/:id', (req, res) => {
 //   connection.query('SELECT * from [회원목록테이블] WHERE [조건]', (error, rows) => {
 //     if (error) throw error;
